a9762c13d4177b97ebecd2f33ccfe06bc623bca7
Jules Olleon <jolleon@gmail.com>
2013-10-03 20:06:04 -0700

    remove old validate stuff

--- sentinel.py
+++ sentinel.py
@@ -1,64 +1,19 @@
 from string import Template
 
-
-class Wrong(Exception):
-
-    def __init__(self, reason, schema, data):
-        msg_template = Template('Invalid $invalid for $path\n expected: $schema\n   actual: $data')
-        message = msg_template.substitute(invalid=self.invalid, path=path, schema=schema, data=data)
-        Exception.__init__(self, message)
-
-
-class WrongType(Wrong):
-    invalid = 'type'
-
-    def __init__(self, path, schema, data):
-        Wrong.__init__(self, path=path, schema=type(schema), data=type(data))
-
-
-class WrongLength(Wrong):
-    invalid = 'length'
-
-    def __init__(self, path, schema, data):
-        Wrong.__init__(self, path=path, schema=len(schema), data=len(data))
-
-
-class WrongKeys(Wrong):
-    invalid = 'keys'
-
-    def __init__(self, path, schema, data):
-        Wrong.__init__(self, path=path, schema=schema.keys(), data=data.keys())
-
-
-def validate(schema, data, prefix='<root>'):
-    if type(schema) is not type(data):
-        raise WrongType(prefix, schema, data)
-
-    if type(schema) is list:
-        if len(schema) != len(data):
-            raise WrongLength(prefix, schema, data)
-        for (schema_item, item) in zip(schema, data):
-            validate(schema_item, item, prefix=prefix + '.[]')
-
-    if type(schema) is dict:
-        if sorted(schema.keys()) != sorted(data.keys()):
-            raise WrongKeys(prefix, schema, data)
-        for key in schema.keys():
-            validate(schema[key], data[key], prefix=prefix + '.' + str(key))
+class Invalid(Exception):
+    pass
 
 
 class Schema(object):
 
-    def __init__(self, schema):
-        self.schema = schema
-
     def serialize(self, data):
-        validate(self.schema, data)
+        problems = self.validate(data)
+        if len(problems) > 0:
+            raise Invalid(problems)
         return data
 
     def deserialize(self, data):
-        validate(self.schema, data)
-        return data
+        return self.serialize(data)
 
 
 class Problem(object):
@@ -75,7 +30,7 @@ class Problem(object):
             self.path = str(node)
 
     def __repr__(self):
-        return "{path}: {reason}\n expected: {expected}\n   actual: {actual}".format(
+        return "{path}: {reason} (expected={expected},  actual={actual})".format(
             path=self.path,
             reason=self.reason,
             expected=self.expected,
--- tests.py
+++ tests.py
@@ -1,73 +1,23 @@
 import unittest
+from mock import Mock
+
 from sentinel import *
 
-class TestValidateValid(unittest.TestCase):
 
-    def test_integer(self):
-        validate(0, 0)
-        validate(1, 1)
-        validate(0, 1)
-        validate(1, 2)
-        validate(-30, 2)
+class TestSchema(unittest.TestCase):
 
-    def test_string(self):
-        validate('', '')
-        validate('a', 'a')
-        validate('a', 'b')
-        validate('a', 'dhajkl')
+    def test_serialize_invalid(self):
+        schema = Schema()
+        schema.validate = Mock()
+        schema.validate.return_value = [Mock()]
+        self.assertRaises(Invalid, schema.serialize, Mock())
 
-    def test_list(self):
-        validate([], [])
-        validate([1], [213])
-
-    def test_dict(self):
-        validate({}, {})
-        validate({1: 1}, {1: 2})
-        validate(
-            {'aa': 1, 2: 'asd'},
-            {'aa': 4, 2: 'jdksla'}
-        )
-
-    def test_none(self):
-        validate(None, None)
-
-
-class TestValidateInvalid(unittest.TestCase):
-
-    def _test_wrongs(self, exc, wrongs):
-        for a in wrongs:
-            for b in wrongs:
-                if a is not b:
-                    self.assertRaises(exc, validate, a, b)
-
-    def test_wrong_type(self):
-        invalid_types = [
-            1,
-            None,
-            'asd',
-            [1],
-            ['a'],
-            [None],
-            {1: 'b'},
-        ]
-        self._test_wrongs(WrongType, invalid_types)
-
-    def test_wrong_length(self):
-        wrong_lengths = [
-            [],
-            [1],
-            [1, 2],
-        ]
-        self._test_wrongs(WrongLength, wrong_lengths)
-
-    def test_wrong_keys(self):
-        wrong_keys = [
-            {},
-            {1: 3},
-            {2: 'a'},
-            {1: 1, 2: 2},
-        ]
-        self._test_wrongs(WrongKeys, wrong_keys)
+    def test_serialize_valid(self):
+        schema = Schema()
+        schema.validate = Mock()
+        schema.validate.return_value = []
+        data = Mock()
+        self.assertEqual(data, schema.serialize(data))
 
 
 if __name__ == '__main__':
